2457d9c6b6fb41a38ca0e182a71d76e69c7138aa
Muhamadnursidik <adesidik200@gmail.com>
2025-08-11 15:10:54 +0700

    update

---
+++ database/migrations/2025_08_11_075402_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('field_id')->constrained('fields')->onDelete('cascade');
+            $table->date('bookking_date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->integer('total_price');
+            $table->enum('status',['avaible','pending','paid','completed','cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
